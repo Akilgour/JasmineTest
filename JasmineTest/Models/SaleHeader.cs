45fd8e57f7c39ec5bf7d8113f839885073867fe3
ashley <kilgourkilgour@hotmail.com>
2016-09-26 13:26:38 +0100

    Added sales header and sales item model

---
+++ JasmineTest/Models/SaleHeader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JasmineTest.Models
+{
+    public class SaleHeader
+    {
+        public int SaleHeaderId { get; set; }
+
+        public string CustomerFirstName { get; set; }
+
+        public string CustomerSecondName { get; set; }
+
+        public string Address { get; set; }
+
+        public virtual List<SaleItem> SaleIem { get; set; }
+    }
+}
---
+++ JasmineTest/Models/SaleItem.cs
@@ -0,0 +1,17 @@
+﻿namespace JasmineTest.Models
+{
+    public class SaleItem
+    {
+        public int SaleItemId { get; set; }
+
+        public string Title { get; set; }
+
+        public int Quantity { get; set; }
+
+        public double CostPerItem { get; set; }
+
+        public int SaleHeaderId { get; set; }
+
+        public virtual SaleHeader SaleHeader { get; set; }
+    }
+}
