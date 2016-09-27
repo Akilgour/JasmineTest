0eb170343ce78350ffcbedbd4c0424eb8e9ad795
ashley <kilgourkilgour@hotmail.com>
2016-09-27 14:09:54 +0100

    Addedd Customer full name view

--- JasmineTest/Models/SaleHeader.cs
+++ JasmineTest/Models/SaleHeader.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -16,5 +18,9 @@ namespace JasmineTest.Models
         public string Address { get; set; }
 
         public virtual List<SaleItem> SaleIem { get; set; }
+
+        [NotMapped] // This will stop entity framework looking at this property
+        [DisplayName("Customer Name")] // This will the caption the view uses
+        public string CustomerFullName { get { return string.Format("{0} {1}", CustomerFirstName, CustomerSecondName); } }
     }
 }
